f26285ccc8f3f2104f465c5c9bf83ddcde266592
Shubham <gogrishubham85@gmail.com>
2023-09-28 14:23:27 +0100

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,24 @@
+# sys -> manipulate the exceptions in the python libraries.
+import sys 
+import logging
+
+def error_message_detail(error, error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    # exec_tb consists the specific details about the occurences of the execption.. i.e. what line it is, which file is it, etc. 
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name, 
+        exc_tb.tb_lineno, 
+        str(error)
+        )
+    return error_message
+    
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
